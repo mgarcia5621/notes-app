547242f492e48d31e90927f6feecb85d3bca188a
Matthew Garcia <matthew27garcia@gmail.com>
2022-04-16 15:23:43 -0500

    added the close

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const addBox = document.querySelector(".add-box"),
 popupBox = document.querySelector(".popup-box"),
-closeIcon = document.querySelector("header i");
+closeIcon = document.querySelector("header i"),
+addBtn = popupBox.querySelector("button");
 
 addBox.addEventListener("click", () => {
     popupBox.classList.add("show");
@@ -8,4 +9,8 @@ addBox.addEventListener("click", () => {
 
 closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show");
+});  
+
+addBtn.addEventListener("click", () => {
+    console.log("Button clicked");
 });  
